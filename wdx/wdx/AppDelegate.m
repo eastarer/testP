be4c1e10dbb57d93e3199ca260b93b68ab3f01ce
王东星 <wangdongxing@wangdongxingdemacbook-pro.local>
2016-07-01 06:50:51 +0800

    ddd

--- wdx/wdx/AppDelegate.m
+++ wdx/wdx/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+//    CGPDFContextAddDocumentMetadata(<#CGContextRef  _Nullable context#>, <#CFDataRef  _Nullable metadata#>)
     return YES;
 }
 
